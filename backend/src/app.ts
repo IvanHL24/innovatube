ea362a6c93eba907b332579d8e915e06ebc24776
IvanHL24 <ivan_hernandez30@outlook.com>
2026-08-09 09:58:50 -0600

    configuración contenedor para backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,11 +8,9 @@ import authRoutes from "./routes/auth.routes";
 import youtubeRoutes from "./routes/youtube.routes";
 import favoritesRoutes from "./routes/favorite.routes";
 
-
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 
 app.use("/users", userRoutes);
@@ -23,8 +21,8 @@ app.use("/videos", youtubeRoutes);
 
 app.use("/favorites", favoritesRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = parseInt(process.env.PORT ?? '3000', 10);
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor ejecutandose en puerto ${PORT}`)
 });
